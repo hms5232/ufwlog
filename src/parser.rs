cdfab45863d1cf8028733d1a9aa03bea7349932b
hms5232 <hms5232@hhming.moe>
2024-04-09 21:04:48 +0800

    wip: log field struct

--- src/parser.rs
+++ src/parser.rs
@@ -35,3 +35,68 @@ pub fn read_lines(path: &str) -> Vec<String> {
 
     file.unwrap().lines().map(String::from).collect()
 }
+
+/// An ufw log
+///
+/// Each field mean can see the following site:
+/// https://help.ubuntu.com/community/UFW#Interpreting_Log_Entries
+/// https://unix.stackexchange.com/a/702909
+struct UfwLog {
+    origin: String,
+
+    month: u8,
+    day: u8,
+    time: String,
+    hostname: String,
+    uptime: String,
+    action: UfwAction,
+
+    r#in: Option<String>,
+    out: Option<String>,
+    mac: Option<String>,
+    src: String,
+    dst: String,
+    len: u32,
+    tos: String, // type of service
+    prec: String,
+    ttl: u16,
+    id: u32,
+    df: bool, // don't fragment
+    proto: String,
+    spt: u16,    // source port
+    dpt: u16,    // detestation port
+    window: u32, // the size of packet the sender is willing to receive
+    res: String,
+
+    // control bits / flag
+    syn: bool,
+    ack: bool,
+    fin: bool,
+    rst: bool,
+    psh: bool,
+    cwr: bool,
+    ece: bool,
+    urgp: bool,
+
+    tc: bool,
+    hoplimit: Option<u8>, // hop limit
+    flowbl: Option<i32>,  // TODO: type need check
+    r#type: Option<i32>,  // TODO: type need check
+    code: Option<String>, // TODO: type check need
+    seq: Option<u32>,
+    mtu: Option<u16>,
+    mark: Option<String>, // TODO: type check need
+
+    // unconfirmed
+    physin: Option<String>,
+    phyout: Option<String>,
+}
+
+/// The ufw action list
+enum UfwAction {
+    Black,
+    Allow,
+    Deny,
+    Audit,
+    AuditInvalid, // AUDIT INVALID
+}
