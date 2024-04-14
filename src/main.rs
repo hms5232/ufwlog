e3d33d9d8a4aecce99c1440c345b8b56d3b66ef0
hms5232 <hms5232@hhming.moe>
2024-04-14 19:21:34 +0800

    feat: cli subcommand "export"

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,87 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
 mod csv;
 mod parser;
 
 fn main() {
-    let mut ufw_log_map = vec![];
-    let log_by_line = parser::read_lines("./ufw.log"); // TODO: accept path flag/argument
-    for i in 0..log_by_line.len() {
-        let map = parser::to_hashmap(&log_by_line[i]);
-        ufw_log_map.push(map);
+    // parse cli subcommand, arguments and flags
+    let cli = Cli::parse();
+
+    // handle subcommand
+    match &cli.command {
+        // export
+        Some(SubCommands::Export {
+            format,
+            log_path,
+            output_filename,
+        }) => {
+            let mut ufw_log_map = vec![];
+            let log_by_line = parser::read_lines(log_path.clone().unwrap().as_str());
+            for i in 0..log_by_line.len() {
+                let map = parser::to_hashmap(&log_by_line[i]);
+                ufw_log_map.push(map);
+            }
+
+            // export with specific format
+            match *format {
+                Some(ExportFormat::Csv) => crate::csv::convert(
+                    ufw_log_map,
+                    Some(output_filename.clone().unwrap().as_str()),
+                )
+                .unwrap(),
+                _ => println!("Current not support other format"),
+            }
+        }
+        _ => {
+            todo!("Default behavior.")
+        }
     }
-    crate::csv::convert(ufw_log_map, None).unwrap();
+}
+
+#[derive(Parser)]
+#[command(name = "ufwlog", version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<SubCommands>,
+}
+
+#[derive(Subcommand)]
+enum SubCommands {
+    /// Export UFW log file with other format
+    Export {
+        /// Which type to be export.
+        #[arg(default_value = "csv")]
+        format: Option<ExportFormat>,
+
+        // if linux, default is read ufw log path
+        // else, read current directory "ufw.log" file
+        #[cfg(target_os = "linux")]
+        /// Specify a log file.
+        #[arg(
+            short,
+            long,
+            value_name = "log_path",
+            default_value = "/var/log/ufw.log"
+        )]
+        log_path: Option<String>,
+        #[cfg(not(target_os = "linux"))]
+        /// Specify a log file.
+        #[arg(short, long, value_name = "log_path", default_value = "./ufw.log")]
+        log_path: Option<String>,
+
+        /// Specify output filename.
+        #[arg(
+            short,
+            long = "output",
+            value_name = "filename",
+            default_value = "ufwlog"
+        )]
+        output_filename: Option<String>,
+    },
+}
+
+/// Supported export format.
+#[derive(Debug, Clone, ValueEnum)]
+enum ExportFormat {
+    Csv,
 }
