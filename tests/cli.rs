8fa719d5f9b12cf4777c49c8d73d20145cb0ff9a
hms5232 <hms5232@hhming.moe>
2025-02-10 20:47:50 +0800

    test: add unknown subcommand output test

--- tests/cli.rs
+++ tests/cli.rs
@@ -11,3 +11,13 @@ fn run_without_args_should_show_help_info() {
     assert!(output.contains("completion")); // subcommand
     assert!(output.contains("help"));
 }
+
+#[test]
+fn unknown_subcommand_would_show_usage() {
+    let mut cmd = Command::cargo_bin("ufwlog").unwrap();
+    cmd.arg("unknown"); // call a subcommand named "unknown" and not exist
+    cmd.assert().failure();
+    let output = String::from_utf8(cmd.output().unwrap().stderr).unwrap();
+    assert!(output.contains("ufwlog")); // binary name
+    assert!(output.contains("--help"));
+}
